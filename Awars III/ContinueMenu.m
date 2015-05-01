8df4a8f570ac47c8879d8f66d374fc2218e43ff8
桐谷 諭史 <Killery@kiritani-no-Mac-mini.local>
2015-05-01 18:34:42 +0900

    new

---
+++ Awars III/ContinueMenu.m
@@ -0,0 +1,21 @@
+//
+//  ContinueMenu.m
+//  Awars III
+//
+//  Created by Killery on 2012/12/06.
+//  Copyright (c) 2012年 Killery. All rights reserved.
+//
+
+#import "ContinueMenu.h"
+
+@implementation ContinueMenu
+
+-(IBAction)Load:(id)sender{
+
+}
+
+-(IBAction)Back:(id)sender{
+    [ContinueMenuWindow close];
+}
+
+@end
