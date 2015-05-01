6ad4a760dbbbb437e1c3830b2695374bab3e8f4e
桐谷 諭史 <Killery@kiritani-no-Mac-mini.local>
2015-05-01 18:36:10 +0900

    new

---
+++ Awars III/BattleView.h
@@ -0,0 +1,14 @@
+//
+//  BattleView.h
+//  Awars III
+//
+//  Created by Killery on 2013/03/23.
+//  Copyright (c) 2013年 Killery. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "FieldScene.h"
+
+@interface BattleView : NSView
+
+@end
