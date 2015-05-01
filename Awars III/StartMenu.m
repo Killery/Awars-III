275aefe07ed05ae37c102b407d8a91aef92c4990
桐谷 諭史 <Killery@kiritani-no-Mac-mini.local>
2015-05-01 18:34:22 +0900

    new

---
+++ Awars III/StartMenu.m
@@ -0,0 +1,22 @@
+//
+//  StartMenu.m
+//  Awars III
+//
+//  Created by Killery on 2012/12/06.
+//  Copyright (c) 2012年 Killery. All rights reserved.
+//
+
+#import "StartMenu.h"
+
+@implementation StartMenu
+
+-(IBAction)Start:(id)sender{
+
+}
+
+-(IBAction)Back:(id)sender{
+    [StartMenuWindow close];
+}
+
+
+@end
